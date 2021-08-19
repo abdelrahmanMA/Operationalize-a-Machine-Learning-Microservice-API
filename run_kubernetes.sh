41073eb9494a7bc45db03bd6aabe53903a29ea0f
abdelrahmanMA <bme.abdelrahmanmuhammed@gmail.com>
2021-08-20 01:06:47 +0200

    add kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,17 +2,14 @@
 
 # This tags and uploads an image to Docker Hub
 
-# Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="abdelrahmanma/udacity-devops"
 
-# Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run udacity-devops-demo --image=$dockerpath --port=80 --labels app=udacity-devops-demo
 
-
-# Step 3:
 # List kubernetes pods
+kubectl get pods
 
-# Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity-devops-demo 8000:80
